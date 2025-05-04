fe4f2056e1e34b731aa45b11b0ce4ef19f42d5df
harun.rasyid <harunrasyid98@gmail.com>
2025-05-04 11:12:29 +0700

    - Feat: remove unused const

--- src/modules/pond/pages/PondPage/PondPage.tsx
+++ src/modules/pond/pages/PondPage/PondPage.tsx
@@ -4,27 +4,6 @@ import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { styles } from "./PondPage.style";
 
-const THEMES = {
-  underwater: {
-    key: "underwater",
-    skyColor: "#309BFF",
-    sunColor: "#FE9E40",
-    groundColor: "#DDD6F3",
-    title: "Underwater",
-    subtitle: "World",
-    models: [
-      `Koi_01`,
-      `Koi_02`,
-      `Koi_03`,
-      `Koi_04`,
-      `Koi_05`,
-      `Koi_06`,
-      `Koi_07`,
-    ],
-    dof: true,
-  },
-};
-
 export const PondPage = () => {
   return (
     <VStack sx={styles.page}>
